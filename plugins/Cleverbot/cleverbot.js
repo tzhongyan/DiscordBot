e084f3b7e55e715ae368c5471c4fa788584d94c4
Tan Zhong Yan <iam@tzhongyan.com>
2017-07-20 18:49:46 +0800

    Fixing for latest cleverbot-node and request dependencies

--- plugins/Cleverbot/cleverbot.js
+++ plugins/Cleverbot/cleverbot.js
@@ -1,29 +1,18 @@
 exports.commands = [
-//    "talk"
+    "talk"
 ]
 
 var cleverbot = require("cleverbot-node");
 talkbot = new cleverbot;
-cleverbot.prepare(function() {});
+talkbot.configure({"botapi":"API_GOES_HERE"});
 
 exports.talk = {
     usage: "<message>",
     description: "Talk directly to the bot",
     process: function(bot, msg, suffix) {
         var conv = suffix.split(" ");
-        talkbot.write(conv, function(response) {
-            msg.channel.sendMessage("", {
-                embed: {
-                    color: 0x8698FE,
-                    author: {
-                        name: "Cleverbot",
-                        icon_url: "https://lh5.ggpht.com/DiNbF90a-ecMdyG7c49ARdKdm2mlhLDyNswLcmDm3WM6yDADmMMWtTO1XL96-LCEXIc=w300"
-                    },
-		    timestamp: new Date(),
-                    description: response.message,
-                }
-            }).catch(console.error);
-            msg.react('👌');
+        talkbot.write(suffix, function(response) {
+            msg.channel.send(response.output);
         })
     }
 }
