f77fbbbf2e4540230fa51c4fecb687e4c03ed1ad
Tan Zhong Yan <iam@tzhongyan.com>
2017-07-24 18:49:55 +0800

    remove unusable files and unused packages

--- plugins/Dev/dev.js
+++ plugins/Dev/dev.js
@@ -1,64 +1,10 @@
 exports.commands = [
 	"pullanddeploy",
-	"version",
 	"myid",
 	"userid"
 ]
 
 //a collection of commands primarily useful for developers
-
-exports.pullanddeploy = {
-	description: "bot will perform a git pull master and restart with the new code",
-	process: function(bot,msg,suffix) {
-		msg.channel.send("fetching updates...").then(function(sentMsg){
-			console.log("updating...");
-			var spawn = require('child_process').spawn;
-			var log = function(err,stdout,stderr){
-				if(stdout){console.log(stdout);}
-				if(stderr){console.log(stderr);}
-			};
-			var fetch = spawn('git', ['fetch']);
-			fetch.stdout.on('data',function(data){
-				console.log(data.toString());
-			});
-			fetch.on("close",function(code){
-				var reset = spawn('git', ['reset','--hard','origin/master']);
-				reset.stdout.on('data',function(data){
-					console.log(data.toString());
-				});
-				reset.on("close",function(code){
-					var npm = spawn('npm', ['install']);
-					npm.stdout.on('data',function(data){
-						console.log(data.toString());
-					});
-					npm.on("close",function(code){
-						console.log("goodbye");
-						sentMsg.edit("brb!").then(function(){
-							bot.destroy().then(function(){
-								process.exit();
-							});
-						});
-					});
-				});
-			});
-		});
-	}
-}
-
-exports.version = {
-	description: "returns the git commit this bot is running",
-	process: function(bot,msg,suffix) {
-		var commit = require('child_process').spawn('git', ['log','-n','1']);
-		commit.stdout.on('data', function(data) {
-			msg.channel.send(data);
-		});
-		commit.on('close',function(code) {
-			if( code != 0){
-				msg.channel.send("failed checking git version!");
-			}
-		});
-	}
-}
 
 exports.myid = {
 	description: "returns the user id of the sender",
