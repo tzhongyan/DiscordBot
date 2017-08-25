2518533ef748dfee7b2f7a2061a6eea94271818e
Tan Zhong Yan <iam@tzhongyan.com>
2017-08-25 11:09:54 +0800

    encapsulate cleverbot answer

--- plugins/Cleverbot/cleverbot.js
+++ plugins/Cleverbot/cleverbot.js
@@ -13,8 +13,18 @@ exports.talk = {
     description: "Talk directly to the bot",
     process: function(bot, msg, suffix) {
         var conv = suffix.split(" ");
-        talkbot.write(suffix, function(response) {
-            msg.channel.send(response.output);
-        })
+        talkbot.write(suffix, response =>{
+            msg.channel.send("", {
+                embed: {
+                    color: 0x8698FE,
+                    author: {
+                        name: "Cleverbot",
+                        icon_url: "https://lh5.ggpht.com/DiNbF90a-ecMdyG7c49ARdKdm2mlhLDyNswLcmDm3WM6yDADmMMWtTO1XL96-LCEXIc=w300"
+                    },
+                timestamp: new Date(),
+                    description: response.message,
+                }
+            }).catch(console.error);
+        });
     }
 }
