34a71ca18e8a6bbe8f7d13ab06d7822cbab3ad3a
Tan Zhong Yan <iam@tzhongyan.com>
2017-11-18 17:51:37 +0800

    running discord with express

--- discord_bot.js
+++ discord_bot.js
@@ -384,36 +384,3 @@ if(process.env.bot_token){
 } else {
     console.log("Logging in with user credentials is no longer supported!\nYou can use token based log in with a user account, see\nhttps://discord.js.org/#/docs/main/master/general/updating");
 }
-
-
-
-// Web app (Express + EJS)
-const http = require('http');
-const express = require('express');
-const app = express();
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-const port = process.env.PORT || 5000;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the `public` directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', (request, response) => {
-    // ejs render automatically looks in the views folder
-    response.render('index');
-});
-
-app.listen(port, () => {
-    // will echo 'Our app is running on http://localhost:5000 when run locally'
-    console.log('Our app is running on http://localhost:' + port);
-});
-
-// pings server every 15 minutes to prevent dynos from sleeping
-setInterval(() => {
- http.get('http://discordjs-heroku.herokuapp.com');
-}, 900000);
